c656af1cca632377f238de967661d97a84f21631
Matthew Rudolph <Matthew.Rudolph@users.noreply.github.com>
2015-12-03 17:21:57 +0000

    Changed the SetVersion.ps1 powershell script into a cmdlet.

--- tools/SetVersion.ps1
+++ tools/SetVersion.ps1
@@ -1,77 +1,91 @@
-$globalAssemblyVersion = '17.23.1.1001'
-$releaseType = "Release"
-
-#Regex to match exactly 4 part version:
-$hasAssemblyVersion = "'"+$globalAssemblyVersion+"'" -match '([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)'
-
-#Regex to match 2, 3 or 4 part version:
-#$hasAssemblyVersion = "'"+$globalAssemblyVersion+"'" -match '[0-9]+(\.([0-9]+|\*)){1,3}'
-
-if (!$hasAssemblyVersion)
-{
-	Write-Host "No version found, using 0.1.0.1000 instead."	
-	$major=0
-	$minor=1
-	$patch=0 ## build
-	$build=1000 ## revision
-}
-else
-{
-	$assemblyVersionFormattedCorrectly = $matches[0] -match "(?<major>[0-9]+)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)\.(?<build>[0-9]+)"
-	##$assemblyVersionFormattedCorrectly = $matches[0] -match "(?<major>[0-9]+)\.(?<minor>[0-9])+(\.(?<patch>([0-9])))?(\.(?<build>([0-9])))?"
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$True)]
+	[string][alias("s")]
+	$version,
 	
-	if (!$assemblyVersionFormattedCorrectly) 
+	[Parameter(Mandatory=$False)]
+	[string][alias("t")]
+	$releaseType = "Release"
+)
+
+function SetAssemblyVersion()
+{
+	#$version = '17.23.2.1004';
+	#$releaseType = "Alpha";
+
+	#Regex to match exactly 4 part version:
+	$hasVersion = "'"+$version+"'" -match '([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)';
+
+	if (!$hasVersion)
 	{
-		Write-Host "The supplied version is not formatted correctly: " $globalAssemblyVersion
-		return;
+		Write-Output "No version found, using 0.1.0.1000 instead.";
+		$major=0;
+		$minor=1;
+		$patch=0;
+		$build=1000;
 	}
-	
-	$major=$matches['major'] -as [int]
-	$minor=$matches['minor'] -as [int]
-	$patch=$matches['patch'] -as [int]
-	$build=$matches['build'] -as [int]
-}
-
-$AssemblyVersion = "$major.$minor.$patch.0"
-$AssemblyFileVersion = "$major.$minor.$patch.$build"
-$AssemblyInformationalVersion = "$major.$minor.$patch-$releaseType$build"
-$NugetVersion = "$major.$minor.$patch"
-if ($releaseType -notmatch "^Release$")
-{
-	$NugetVersion = "$major.$minor.$patch-$releaseType$build"
-}
-
-Write-Host "Using Assembly Version: $AssemblyVersion"
-Write-Host "Using File Version: $AssemblyFileVersion"
-Write-Host "Using Informational Version: $AssemblyInformationalVersion"
-Write-Host "Using Nuget Version: $nugetVersion"
-
-#$AssemblyFileVersion = "$major.$minor.$env:APPVEYOR_BUILD_NUMBER"
-#$AssemblyInformationalVersion = "$AssemblyFileVersion-$env:APPVEYOR_REPO_SCM" + ($env:APPVEYOR_REPO_COMMIT).Substring(0, 8)
-
-
-$fileAssemblyVersion = 'AssemblyVersion("' + $AssemblyVersion + '")';
-$fileFileVersion = 'AssemblyFileVersion("' + $AssemblyFileVersion + '")';
-$fileInformationalVersion = 'AssemblyInformationalVersion("' + $AssemblyInformationalVersion + '")';
-
-$foundFiles = get-childitem .\*AssemblyInfo.cs -recurse  
-foreach( $file in $foundFiles )  
-{
-	if ($file.Name -eq $globalAssemblyFile)
+	else
 	{
-		#Don't patch the global info.
-		continue;
+		$assemblyVersionFormattedCorrectly = $matches[0] -match "(?<major>[0-9]+)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)\.(?<build>[0-9]+)"
+	
+		if (!$assemblyVersionFormattedCorrectly) 
+		{
+			Write-Output "The supplied version is not formatted correctly: " $version
+			return;
+		}
+	
+		$major=$matches['major'] -as [int]
+		$minor=$matches['minor'] -as [int]
+		$patch=$matches['patch'] -as [int]
+		$build=$matches['build'] -as [int]
 	}
 
-	$content = Get-Content "$file"
+	$AssemblyVersion = "$major.$minor.$patch.0"
+	$AssemblyFileVersion = "$major.$minor.$patch.$build"
+	$AssemblyInformationalVersion = "$major.$minor.$patch-$releaseType$build"
+	$NugetVersion = "$major.$minor.$patch"
+	if ($releaseType -notmatch "^Release$")
+	{
+		$NugetVersion = "$major.$minor.$patch-$releaseType$build"
+	}
+
+	Write-Output "Using Assembly Version: $AssemblyVersion"
+	Write-Output "Using File Version: $AssemblyFileVersion"
+	Write-Output "Using Informational Version: $AssemblyInformationalVersion"
+	Write-Output "Using Nuget Version: $nugetVersion"
+
+	#$AssemblyFileVersion = "$major.$minor.$env:APPVEYOR_BUILD_NUMBER"
+	#$AssemblyInformationalVersion = "$AssemblyFileVersion-$env:APPVEYOR_REPO_SCM" + ($env:APPVEYOR_REPO_COMMIT).Substring(0, 8)
+
+
+	$fileAssemblyVersion = 'AssemblyVersion("' + $AssemblyVersion + '")';
+	$fileFileVersion = 'AssemblyFileVersion("' + $AssemblyFileVersion + '")';
+	$fileInformationalVersion = 'AssemblyInformationalVersion("' + $AssemblyInformationalVersion + '")';
+
+	$foundFiles = get-childitem .\*AssemblyInfo.cs -recurse  
+	foreach( $file in $foundFiles )  
+	{
+		if ($file.Name -eq $globalAssemblyFile)
+		{
+			#Don't patch the global info.
+			continue;
+		}
+
+		$content = Get-Content "$file"
 	
-	Write-Host "Patching $file"
+		Write-Output "Patching $file"
 	
-	$av = $fileAssemblyVersion
-	$afv = $fileFileVersion
-	$aiv = $fileInformationalVersion	
+		$av = $fileAssemblyVersion
+		$afv = $fileFileVersion
+		$aiv = $fileInformationalVersion	
 	
-	$content -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $av `
-			 -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $afv `
-			 -replace 'AssemblyInformationalVersion\("[0-9]+(\.([0-9]+|\*)){1,3}.*"\)', $aiv | Set-Content "$file"
+		$content -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $av `
+				 -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $afv `
+				 -replace 'AssemblyInformationalVersion\("[0-9]+(\.([0-9]+|\*)){1,3}.*"\)', $aiv | Set-Content "$file"
+	
+		Write-Output "Completed patching $file"
+	}
 }
+
+SetAssemblyVersion;
