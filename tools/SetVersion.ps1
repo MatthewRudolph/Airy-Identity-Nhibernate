2370cf008786c1a3ef8f958b6d5a011c04bfdf52
Matthew Rudolph <Matthew.Rudolph@users.noreply.github.com>
2015-12-03 17:49:44 +0000

    Updates to the SetVersion powershell cmdlet.

--- tools/SetVersion.ps1
+++ tools/SetVersion.ps1
@@ -24,45 +24,45 @@ function CalculateVersions()
 	}
 	else
 	{
-		$versionFormattedCorrectly = $matches[0] -match "(?<major>[0-9]+)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)\.(?<build>[0-9]+)"	
-		if (!$versionFormattedCorrectly) 
+		$versionFormattedCorrectly = $matches[0] -match "(?<major>[0-9]+)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)\.(?<build>[0-9]+)";
+		if (!$versionFormattedCorrectly)
 		{
-			Write-Output "The supplied version is not formatted correctly: " $version
-			return;
+			Write-Output "The supplied version is not formatted correctly: " $version;
+			exit 1;
 		}
 	
-		$major=$matches['major'] -as [int]
-		$minor=$matches['minor'] -as [int]
-		$patch=$matches['patch'] -as [int]
-		$build=$matches['build'] -as [int]
+		$major=$matches['major'] -as [int];
+		$minor=$matches['minor'] -as [int];
+		$patch=$matches['patch'] -as [int];
+		$build=$matches['build'] -as [int];
 	}
 
-	$Script:AssemblyVersion = "$major.$minor.$patch.0"
-	$Script:AssemblyFileVersion = "$major.$minor.$patch.$build"
-	$Script:AssemblyInformationalVersion = "$major.$minor.$patch-$releaseType$build"
-	$Script:NugetVersion = "$major.$minor.$patch"
+	$Script:AssemblyVersion = "$major.$minor.$patch.0";
+	$Script:AssemblyFileVersion = "$major.$minor.$patch.$build";
+	$Script:AssemblyInformationalVersion = "$major.$minor.$patch-$releaseType$build";
+	$Script:NugetVersion = "$major.$minor.$patch";
 	if ($releaseType -notmatch "^Release$")
 	{
-		$Script:NugetVersion = "$major.$minor.$patch-$releaseType$build"
+		$Script:NugetVersion = "$major.$minor.$patch-$releaseType$build";
 	}
 
-	Write-Output "Using Assembly Version: $AssemblyVersion"
-	Write-Output "Using File Version: $AssemblyFileVersion"
-	Write-Output "Using Informational Version: $AssemblyInformationalVersion"
-	Write-Output "Using Nuget Version: $nugetVersion"
+	Write-Output "Using Assembly Version: $AssemblyVersion";
+	Write-Output "Using File Version: $AssemblyFileVersion";
+	Write-Output "Using Informational Version: $AssemblyInformationalVersion";
+	Write-Output "Using Nuget Version: $nugetVersion";
 }
 
 function SetAssemblyVersion()
 {
-	#$AssemblyFileVersion = "$major.$minor.$env:APPVEYOR_BUILD_NUMBER"
-	#$AssemblyInformationalVersion = "$AssemblyFileVersion-$env:APPVEYOR_REPO_SCM" + ($env:APPVEYOR_REPO_COMMIT).Substring(0, 8)
+	#$AssemblyFileVersion = "$major.$minor.$env:APPVEYOR_BUILD_NUMBER";
+	#$AssemblyInformationalVersion = "$AssemblyFileVersion-$env:APPVEYOR_REPO_SCM" + ($env:APPVEYOR_REPO_COMMIT).Substring(0, 8);
 
 	$fileAssemblyVersion = 'AssemblyVersion("' + $AssemblyVersion + '")';
 	$fileFileVersion = 'AssemblyFileVersion("' + $AssemblyFileVersion + '")';
 	$fileInformationalVersion = 'AssemblyInformationalVersion("' + $AssemblyInformationalVersion + '")';
 
-	$foundFiles = get-childitem .\*AssemblyInfo.cs -recurse  
-	foreach( $file in $foundFiles )  
+	$foundFiles = get-childitem .\*AssemblyInfo.cs -recurse
+	foreach( $file in $foundFiles )
 	{
 		if ($file.Name -eq $globalAssemblyFile)
 		{
@@ -70,21 +70,15 @@ function SetAssemblyVersion()
 			continue;
 		}
 
-		$content = Get-Content "$file"
-	
-		Write-Output "Patching $file"
-	
-		$av = $fileAssemblyVersion
-		$afv = $fileFileVersion
-		$aiv = $fileInformationalVersion	
-	
-		$content -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $av `
-				 -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $afv `
-				 -replace 'AssemblyInformationalVersion\("[0-9]+(\.([0-9]+|\*)){1,3}.*"\)', $aiv | Set-Content "$file"
-	
-		Write-Output "Completed patching $file"
+		$content = Get-Content "$file";
+		Write-Output "Patching $file";
+		$content -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $fileAssemblyVersion `
+				 -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $fileFileVersion `
+				 -replace 'AssemblyInformationalVersion\("[0-9]+(\.([0-9]+|\*)){1,3}.*"\)', $fileInformationalVersion | Set-Content "$file";
+		Write-Output "Completed patching $file";
 	}
 }
 
 CalculateVersions;
 SetAssemblyVersion;
+Exit 1;
