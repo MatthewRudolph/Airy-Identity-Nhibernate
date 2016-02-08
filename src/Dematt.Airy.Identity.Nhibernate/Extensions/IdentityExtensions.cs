5a05d82800dbfdb926e76aa7c2bf9adbb54432ec
Matthew Rudolph <Matthew.Rudolph@users.noreply.github.com>
2016-02-08 14:17:29 +0000

    Added extension method to IIdentity to parse out the string Id value from the claims into a guid, as the default ASP.Net Identity extension method User.Identity.GetUserId<>() will not work for Guids as they don't implement IConvertible.

---
+++ src/Dematt.Airy.Identity.Nhibernate/Extensions/IdentityExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Principal;
+using Microsoft.AspNet.Identity;
+
+namespace Dematt.Airy.Identity.Nhibernate.Extensions
+{
+    public static class IdentityExtensions
+    {
+        /// <summary>
+        /// <![CDATA[The default ASP.Net Identity extension method User.Identity.GetUserId<>() will not work for Guids as they don't implement IConvertible.]]>
+        /// <![CDATA[This extension method will attempt to parse out the string Id value into a guid, if it can't parse it will return Guid.Empty.]]>
+        /// </summary>
+        public static Guid GetGuidUserId(this IIdentity identity)
+        {
+            Guid result;
+            Guid.TryParse(identity.GetUserId(), out result);
+            return result;
+        }
+    }
+}
