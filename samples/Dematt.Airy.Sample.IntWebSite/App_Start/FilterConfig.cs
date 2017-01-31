ac60b994b9207c4bac74b653caffe9cdac1e3d7c
Matthew Rudolph <Matthew.Rudolph@users.noreply.github.com>
2017-01-31 16:27:29 +0000

    Add sample project for int id sample. Project is just default visual studio ASP.NET MVC template project with the local user account authentication selected, no changes yet.

---
+++ samples/Dematt.Airy.Sample.IntWebSite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Dematt.Airy.Sample.IntWebSite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ samples/Dematt.Airy.Sample.IntWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dematt.Airy.Sample.IntWebSite.Startup))]
+namespace Dematt.Airy.Sample.IntWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
